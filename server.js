b8043a7335ad8b03cfbf5f1794acf63b620d3b06
Vo <vo@Vos-iMac.local>
2018-08-09 19:39:41 +1000

    Add project page

--- server.js
+++ server.js
@@ -40,6 +40,12 @@ app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
   });
+});
+
+app.get('/projects',(req, res) =>{
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
 })
 
 app.listen(port, () =>{
